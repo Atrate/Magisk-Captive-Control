95c77aba8c7eb6e37a541c8822b1cbb02d5e9b34
Atrate <Atrate@protonmail.com>
2020-07-25 01:11:58 +0200

    Add copyright notice
    Signed-off-by: Atrate <Atrate@protonmail.com>

--- service.sh
+++ service.sh
@@ -1,3 +1,14 @@
+# Copyright (C) 2020 Atrate <atrate@protonmail.com>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 #!/system/bin/sh
 
 # Mode: 1 — Just change the Captive Portal URL, 0 — Completely disable Captive Portal detection
